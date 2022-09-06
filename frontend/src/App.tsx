bc5865155f186c555a3f5d2ae948b9ed0a915440
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2022-09-05 22:44:46 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
- return (
-  <h1>Olá Dev</h1>
- )
+  return (
+    <>
+      <h1>Olá Dev</h1>
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
